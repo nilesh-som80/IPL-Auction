926012ea936e610da2eb3ac4886cf352014d0e41
Nilesh Singh <69386843+nilesh-som80@users.noreply.github.com>
2021-07-11 19:11:47 +0530

    Revert "First Commit"
    This reverts commit 7bf43d657ab9ec4301e0fd582c8459b4b363ce22.

--- esummit21/productdetail/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Player,TEST_STATS,ODI_STATS,T20I_STATS,IPL_STATS
-# Register your models here.
-
-admin.site.register(Player)
-admin.site.register(TEST_STATS)
-admin.site.register(ODI_STATS)
-admin.site.register(T20I_STATS)
-admin.site.register(IPL_STATS)
